7ad0da8de78b47edeb033529770901bff2973ce4
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-02-17 11:27:05 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module com.example.irrigationapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires javafx.graphics;
+
+    opens view to javafx.fxml;
+    exports view;
+    exports controller;
+    opens controller to javafx.fxml;
+}
