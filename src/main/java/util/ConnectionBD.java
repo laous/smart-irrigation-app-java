8d1d60bef8828ebda8ff31223642fa6ea48506c2
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-02-25 21:00:24 +0100

    Commit

--- src/main/java/util/ConnectionBD.java
+++ src/main/java/util/ConnectionBD.java
@@ -1,4 +1,18 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionBD {
+
+    static Connection con;
+
+    static {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/irrigation","root","");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
