d77b0c18b770a20acc502c15f203f77587005eee
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-02-25 09:44:40 +0100

    login controller

--- src/main/java/controller/HandleLogin.java
+++ src/main/java/controller/HandleLogin.java
@@ -1,4 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class HandleLogin {
+    String sampleEmail = "oussama@admin.com";
+    String samplePassword = "irrigation";
+
+    @FXML
+    private Button loginBtn ;
+    @FXML
+    private Label conStatus ;
+    @FXML
+    private TextField email ;
+    @FXML
+    private PasswordField password;
+
+    public void seConnecter(ActionEvent ev) {
+
+        if(sampleEmail.equals(email.getText()) && samplePassword.equals(password.getText())){
+            conStatus.setText("Connection reussie!");
+        }else{
+            conStatus.setText("Connection echoue!");
+        }
+    }
 }
