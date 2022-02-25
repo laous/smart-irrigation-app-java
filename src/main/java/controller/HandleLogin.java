e3f4ad116f56f9d3e83e9b32fd3bfa02708fec23
Oussama Lamnaouer <lmnouer59@gmail.com>
2022-02-25 09:51:32 +0100

    change scene when logged in

--- src/main/java/controller/HandleLogin.java
+++ src/main/java/controller/HandleLogin.java
@@ -3,9 +3,12 @@ package controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import view.HelloApplication;
+
+import java.io.IOException;
 
 public class HandleLogin {
-    String sampleEmail = "oussama@admin.com";
+    String sampleEmail = "admin";
     String samplePassword = "irrigation";
 
     @FXML
@@ -17,10 +20,13 @@ public class HandleLogin {
     @FXML
     private PasswordField password;
 
-    public void seConnecter(ActionEvent ev) {
+    public void seConnecter(ActionEvent ev) throws InterruptedException, IOException {
 
         if(sampleEmail.equals(email.getText()) && samplePassword.equals(password.getText())){
             conStatus.setText("Connection reussie!");
+            Thread.sleep(3000);
+            HelloApplication m = new HelloApplication();
+            m.changeScene("gestionCapteurs.fxml");
         }else{
             conStatus.setText("Connection echoue!");
         }
--- src/main/java/view/HelloApplication.java
+++ src/main/java/view/HelloApplication.java
@@ -2,14 +2,18 @@ package view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+    private static Stage stg;
     @Override
     public void start(Stage stage) throws IOException {
+        stg=stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Smart Irrigation");
@@ -19,5 +23,11 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(fxml)));
+
+        stg.getScene().setRoot(pane);
     }
 }
