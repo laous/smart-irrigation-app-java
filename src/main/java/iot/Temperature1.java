58f1c469e15792b4f18acc0522973ea3ee83eff8
Younes Lamkhanter <younes_lamkhanter@um5.ac.ma>
2022-02-27 17:22:27 +0100

    Update iot clients

--- src/main/java/iot/Temperature1.java
+++ src/main/java/iot/Temperature1.java
@@ -16,24 +16,36 @@ public class Temperature1 {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Capteur c = new CapteurTemperature("APS56","A","2", (float) 7.98);
+        Capteur c = new CapteurTemperature("T4\n", "a", "1");
 
         String hote = "127.0.0.1";
         int port = 3003;
         Socket soc = new Socket(hote, port);
 
         OutputStream streamOut = soc.getOutputStream();
-        OutputStreamWriter sortie = new OutputStreamWriter (streamOut) ;
+        OutputStreamWriter sortie = new OutputStreamWriter(streamOut);
+
+        sendInfosReservoir(sortie,c);
 
         BufferedReader fs = new BufferedReader(new FileReader("D:\\Top Secret\\WebDev\\Java Workspace\\smart-irrigation-app-java\\src\\main\\java\\iot\\temperature1.txt"));
 
-        String data= null;
-        while((data = fs.readLine()) != null){
+        String data = null;
+        while ((data = fs.readLine()) != null) {
             System.out.println(data);
-            TimeUnit.SECONDS.sleep(30); // sleep 30 secs
+            TimeUnit.SECONDS.sleep(5); // sleep 30 secs
             //l'envoyer au serveur temperature
-            sortie.write(data+"\n");
+            sortie.write(data + "\n");
             sortie.flush(); // forcer l'envoi
         }
+    }
+
+    public static void sendInfosReservoir(OutputStreamWriter sortie, Capteur c) throws IOException {
+        System.out.println("Sending infos");
+        sortie.write(c.getCode()+"\n"); // send code
+        sortie.flush();
+        sortie.write(c.getEtat()+"\n"); // send etat
+        sortie.flush();
+        sortie.write(c.getZone()+"\n"); // send zone
+        sortie.flush();
     }
 }
